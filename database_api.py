2833c3e184b20c02f73ecb8eaf06ef695387ad65
Benoit Seguin <seg.benoit@gmail.com>
2016-01-13 18:02:11 +0100

    Cleaning

--- database_api.py
+++ database_api.py
@@ -1,14 +1,5 @@
 from flask import abort
 from flask_restful import Resource, reqparse
-from data_manager import DatabaseElement, DataManager
-import numpy as np
-import os.path
-from matplotlib.pyplot import imread
-import io
-from urllib.request import urlopen
-from urllib.error import URLError
-from signature_extractor import SignatureExtractorManager
-from typing import Dict
 import replica
 import replica.util
 import replica.config
@@ -20,26 +11,6 @@ metadata_parser.add_argument('metadata', type=dict, default=dict(), location='js
 
 image_url_metadata_parser = metadata_parser.copy()
 image_url_metadata_parser.add_argument('image_url', type=str, location='json')
-
-
-def _get_image_url(args: dict) -> str:
-        image_url = args['image_url']
-        if not DataManager.has_url(image_url):
-            abort(400)
-        return image_url
-
-
-def _generate_signatures_from_url(image_url: str) -> Dict[str, np.ndarray]:
-    ext = os.path.splitext(image_url)[1]
-    try:
-        http_response = urlopen(image_url)
-    except URLError:
-        raise ValueError('image_url unreachable : {} '.format(image_url))
-    try:
-        im = imread(io.BytesIO(http_response.read()), ext)
-    except IOError:
-        raise ValueError('Could not decode image : {}'.format(image_url))
-    return SignatureExtractorManager.compute_signatures(im)
 
 
 class DatabaseAPI(Resource):
@@ -60,15 +31,19 @@ class DatabaseAPI(Resource):
         if not is_url_valid:
             abort(400, 'Invalid or unreachable image_url: {}'.format(image_url))
 
+        # Creates the document to be inserted
         new_document = {'image_url': image_url,
                         'origin': 'image-matcher',
                         'metadata': args['metadata']}
 
+        # Tries to insert, abort if key already present
         uuid = replica.util.insert_element_to_database(new_document,
                                                        replica.config.DEFAULT_IMAGES_COLLECTION,
                                                        replica.config.IMAGES_DB)
         if uuid is None:
             abort(400, "Image is already present in the database : {}".format(image_url))
+
+        # Launch the task of the computation of features
         replica.features.features.compute_features_for_img_in_db.delay(uuid)
 
 
@@ -79,7 +54,7 @@ class DatabaseElementAPI(Resource):
         if element is None:
             abort(404, "image_url not found : {}".format('image_url'))
         # TODO Return the info about computed features and indexes
-        return DataManager.get_metadata_from_url(image_url)
+        return element
 
     def put(self, image_url: str):
         args = metadata_parser.parse_args()
