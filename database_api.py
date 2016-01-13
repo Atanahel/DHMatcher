cdd936a15d32713fee208dc03036d26d41e15d4c
Benoit Seguin <seg.benoit@gmail.com>
2016-01-13 17:56:45 +0100

    Update of the database_api to correspond to the new Replica module

--- database_api.py
+++ database_api.py
@@ -9,12 +9,14 @@ from urllib.request import urlopen
 from urllib.error import URLError
 from signature_extractor import SignatureExtractorManager
 from typing import Dict
+import replica
+import replica.util
+import replica.config
+import replica.features.features
+import requests
 
 metadata_parser = reqparse.RequestParser()
-metadata_parser.add_argument('author', type=str, default='', location='json')
-metadata_parser.add_argument('title', type=str, default='', location='json')
-metadata_parser.add_argument('date', type=str, default='', location='json')
-metadata_parser.add_argument('additional_metadata', type=dict, default=dict(), location='json')
+metadata_parser.add_argument('metadata', type=dict, default=dict(), location='json')
 
 image_url_metadata_parser = metadata_parser.copy()
 image_url_metadata_parser.add_argument('image_url', type=str, location='json')
@@ -45,32 +47,47 @@ class DatabaseAPI(Resource):
     def post(self):
         args = image_url_metadata_parser.parse_args()
         image_url = args['image_url']
-        if DataManager.has_url(image_url):
-            abort(400, "image_url already present in the database")
+
+        # Check that there is actually a valid image there
+        is_url_valid = False
         try:
-            signatures = _generate_signatures_from_url(image_url)
-        except ValueError as err:
-            abort(400, str(err))
-        DataManager.add_element(DatabaseElement(args, signatures))
+            r = requests.get(image_url, stream=True,
+                             headers={'User-agent': 'Mozilla/5.0'})
+            is_url_valid = r.headers['Content-Type'].split('/')[0] == "image"
+        except:
+            pass
 
+        if not is_url_valid:
+            abort(400, 'Invalid or unreachable image_url: {}'.format(image_url))
 
-def _check_has_image_url(image_url: str) -> str:
-        if not DataManager.has_url(image_url):
-            abort(404, "image_url not present in the database")
+        new_document = {'image_url': image_url,
+                        'origin': 'image-matcher',
+                        'metadata': args['metadata']}
+
+        uuid = replica.util.insert_element_to_database(new_document,
+                                                       replica.config.DEFAULT_IMAGES_COLLECTION,
+                                                       replica.config.IMAGES_DB)
+        if uuid is None:
+            abort(400, "Image is already present in the database : {}".format(image_url))
+        replica.features.features.compute_features_for_img_in_db.delay(uuid)
 
 
 class DatabaseElementAPI(Resource):
 
     def get(self, image_url: str):
-        _check_has_image_url(image_url)
+        element = replica.util.get_element_from_image_url(image_url)
+        if element is None:
+            abort(404, "image_url not found : {}".format('image_url'))
+        # TODO Return the info about computed features and indexes
         return DataManager.get_metadata_from_url(image_url)
 
     def put(self, image_url: str):
-        _check_has_image_url(image_url)
         args = metadata_parser.parse_args()
-        args['image_url'] = image_url
-        DataManager.set_metadata_from_url(args)
+        modified = replica.util.update_metadata_from_image_url(image_url, args['metadata'])
+        if not modified:
+            abort(404, "image_url not found : {}".format('image_url'))
 
     def delete(self, image_url: str):
-        _check_has_image_url(image_url)
-        DataManager.remove_url(image_url)
+        deleted = replica.util.remove_element_from_image_url(image_url)
+        if not deleted:
+            abort(404, "image_url not found : {}".format('image_url'))
